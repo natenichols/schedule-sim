f9ff6a3fdbab270fd19e110b2bad4b57e96a5a3f
Andre Kurait <andrekurait@gmail.com>
2019-11-01 21:49:10 -0500

    removed nathan's junk files

--- Documents/scheduler/doc/html/annotated_dup.js
+++
@@ -1,6 +0,0 @@
-var annotated_dup =
-[
-    [ "heap_node", "structheap__node.html", "structheap__node" ],
-    [ "job_t", "structjob__t.html", null ],
-    [ "priqueue_t", "structpriqueue__t.html", "structpriqueue__t" ]
-];
--- Documents/scheduler/doc/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['heap_5fnode',['heap_node',['../structheap__node.html',1,'']]]
-];
