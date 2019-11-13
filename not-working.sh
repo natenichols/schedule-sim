3dcd20f23bccf376225624017402e03e90d38f25
Andre Kurait <andrekurait@gmail.com>
2019-11-13 11:36:55 -0600

    added the proc1 core 1 to test scripts

--- not-working.sh
+++ not-working.sh
@@ -1,2 +1,3 @@
 ./simulator -c 1 -s ppri examples/proc1.csv > test.out && diff -u test.out examples/proc1-c1-ppri.out
+./simulator -c 1 -s psjf examples/proc1.csv > test.out && diff -u test.out examples/proc1-c1-psjf.out
 rm test.out
--- working.sh
+++ working.sh
@@ -1,4 +1,10 @@
 ./simulator -c 1 -s fcfs examples/proc1.csv > test.out && diff -u test.out examples/proc1-c1-fcfs.out
+# ./simulator -c 1 -s ppri examples/proc1.csv > test.out && diff -u test.out examples/proc1-c1-ppri.out
 ./simulator -c 1 -s pri examples/proc1.csv > test.out && diff -u test.out examples/proc1-c1-pri.out
+# ./simulator -c 1 -s psjf examples/proc1.csv > test.out && diff -u test.out examples/proc1-c1-psjf.out
+./simulator -c 1 -s rr1 examples/proc1.csv > test.out && diff -u test.out examples/proc1-c1-rr1.out
+./simulator -c 1 -s rr2 examples/proc1.csv > test.out && diff -u test.out examples/proc1-c1-rr2.out
+./simulator -c 1 -s rr4 examples/proc1.csv > test.out && diff -u test.out examples/proc1-c1-rr4.out
+./simulator -c 1 -s sjf examples/proc1.csv > test.out && diff -u test.out examples/proc1-c1-sjf.out
 
 rm test.out
